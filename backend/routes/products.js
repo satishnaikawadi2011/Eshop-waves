49ede9f089d1e84d684df8e7cf46d947224746d4
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-12-07 20:28:58 +0530

    fetched data from database with mongoose and added product routes

---
+++ backend/routes/products.js
@@ -0,0 +1,28 @@
+import express from 'express';
+const router = express.Router();
+import Product from '../models/product.js';
+
+import expressAsyncHandler from 'express-async-handler';
+
+router.get(
+	'/',
+	expressAsyncHandler(async (req, res) => {
+		const products = await Product.find({});
+		res.json(products);
+	})
+);
+
+router.get(
+	'/:id',
+	expressAsyncHandler(async (req, res) => {
+		const product = await Product.findById(req.params.id);
+		if (product) {
+			res.json(product);
+		}
+		else {
+			res.status(404).json({ message: 'Product not found' });
+		}
+	})
+);
+
+export default router;
