6e93322f248919ddf57a35984de493aeb2e9b4b7
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-12-07 22:36:35 +0530

    added Message and Loader components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,35 +1,39 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-// import products from '../products';
-import axios from 'axios';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../redux/actions/products';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 
 const HomeScreen = () => {
-	const [
-		products,
-		setProducts
-	] = useState([]);
-
-	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get('/api/products');
-			setProducts(data);
-		};
-		fetchProducts();
-	}, []);
+	const dispatch = useDispatch();
+	const productList = useSelector((state) => state.productList);
+	const { loading, error, products } = productList;
+	useEffect(
+		() => {
+			dispatch(listProducts());
+		},
+		[
+			dispatch
+		]
+	);
 	return (
 		<React.Fragment>
 			<h1>Latest Products</h1>
-			<Row>
-				{products.map((product) => {
-					return (
-						<Col key={product._id} sm={12} md={6} xl={3}>
-							<Product product={product} />
-						</Col>
-					);
-				})}
-			</Row>
+			{
+				loading ? <Loader /> :
+				error ? <Message variant="danger">{error}</Message> :
+				<Row>
+					{products.map((product) => {
+						return (
+							<Col key={product._id} sm={12} md={6} xl={3}>
+								<Product product={product} />
+							</Col>
+						);
+					})}
+				</Row>}
 		</React.Fragment>
 	);
 };
