d8427ddc37ce4f9a674b81b6fc6090accddc947f
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-12-07 11:31:53 +0530

    Added ProductScreen component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,26 @@ import './bootstrap.min.css';
 import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
+
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
 	return (
 		<React.Fragment>
-			<Header />
-			<main className="py-3">
-				<Container>
-					<HomeScreen />
-				</Container>
-			</main>
-			<Footer />
+			<Router>
+				<Header />
+				<main className="py-3">
+					<Container>
+						<Route exact path="/" component={HomeScreen} />
+						<Route path="/product/:id" component={ProductScreen} />
+					</Container>
+				</main>
+				<Footer />
+			</Router>
 		</React.Fragment>
 	);
 }
