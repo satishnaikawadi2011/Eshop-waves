151d89d6e0a0ead70e1af7505311d47163a1dfb4
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-12-10 15:03:48 +0530

    seperated routes and controllers

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,29 +1,9 @@
 import express from 'express';
+import { getProductById, getProducts } from '../controllers/product.js';
 const router = express.Router();
-import Product from '../models/product.js';
 
-import expressAsyncHandler from 'express-async-handler';
+router.get('/', getProducts);
 
-router.get(
-	'/',
-	expressAsyncHandler(async (req, res) => {
-		const products = await Product.find({});
-		res.json(products);
-	})
-);
-
-router.get(
-	'/:id',
-	expressAsyncHandler(async (req, res) => {
-		const product = await Product.findById(req.params.id);
-		if (product) {
-			res.json(product);
-		}
-		else {
-			res.status(404);
-			throw new Error('Product not found');
-		}
-	})
-);
+router.get('/:id', getProductById);
 
 export default router;
