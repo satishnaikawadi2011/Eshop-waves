13572c7a7a58120c1ce398649b762a92dd787e39
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-12-10 16:26:26 +0530

    added register user route

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -35,3 +35,34 @@ export const getUserProfile = expressAsyncHandler(async (req, res) => {
 		throw new Error('User not found!');
 	}
 });
+
+export const registerUser = expressAsyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+
+	const userExists = await User.findOne({ email });
+	if (userExists) {
+		res.status(400);
+		throw new Error('User already exists,try another email or login instaed!');
+	}
+
+	const user = await User.create({
+		name,
+		email,
+		password
+	});
+
+	if (user) {
+		res.status(201);
+		res.json({
+			_id     : user._id,
+			name    : user.name,
+			email   : user.email,
+			isAdmin : user.isAdmin,
+			token   : generateToken(user._id)
+		});
+	}
+	else {
+		res.json(400);
+		throw new Error('Invalid user data');
+	}
+});
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { authUser, getUserProfile } from '../controllers/user.js';
+import { authUser, getUserProfile, registerUser } from '../controllers/user.js';
 import auth from '../middlewares/auth.js';
 const router = express.Router();
 
 router.post('/login', authUser);
 
 router.get('/profile', auth, getUserProfile);
+
+router.post('/register', registerUser);
 
 export default router;
